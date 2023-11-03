fb1fd37bd79f2272c1d5338db2808728230b53ca
trezorsky <130697823+trezorsky@users.noreply.github.com>
2023-11-03 12:40:27 +0300

    module4_q17

--- src/main/java/ru/evol/eProj/controller/PersonController.java
+++ src/main/java/ru/evol/eProj/controller/PersonController.java
@@ -77,56 +77,6 @@ public class PersonController {
         }
     }
 
-//    @PostMapping("/persons")
-//    public Person addPerson(@RequestBody Person person) {
-//        repositoryPersons.save(person);
-//        return person;
-//    }
-//
-//    @PostMapping("/persons")
-//    public Person addPerson(@RequestBody Person person) {
-//        repository.save(person);
-//        return person;
-//    }
-//
-//    @PostMapping("/persons/{id}/messages")
-//    public Person addMessage(@PathVariable int id, @RequestBody Message message) {
-//        Person person = repository.findById(id).get();
-//        person.addMessage(message);
-//        return repository.save(person);
-//    }
-//
-//    @PutMapping("/persons/{id}")
-//    public ResponseEntity<Person> updatePerson(@PathVariable int id, @RequestBody Person updatedPerson) {
-//        Optional<Person> existingPerson = repository.findById(id);
-//
-//        if (existingPerson.isPresent()) {
-//            Person person = existingPerson.get();
-//            person.setFirstname(updatedPerson.getFirstname());
-//            person.setSurname(updatedPerson.getSurname());
-//            person.setLastname(updatedPerson.getLastname());
-//            person.setBirthday(updatedPerson.getBirthday());
-//            return new ResponseEntity<>(repository.save(person), HttpStatus.OK);
-//        } else {
-//            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-//        }
-//    }
-//
-//    @DeleteMapping("/persons/{id}")
-//    public void deletePerson(@PathVariable int id) {
-//        repository.deleteById(id);
-//    }
-//
-//    @GetMapping("/persons")
-//    public Iterable<Person> getPersons() {
-//        return repository.findAll();
-//    }
-//
-//    @GetMapping("/persons/{id}")
-//    public Optional<Person> findPersonById(@PathVariable int id) {
-//        return repository.findById(id);
-//    }
-
     @GetMapping
     public String hello() {
         return "Hello, World!";
