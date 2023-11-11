048fa50759c29d7cf3ada247079fe20214cce8a4
trezorsky <130697823+trezorsky@users.noreply.github.com>
2023-11-11 14:39:32 +0300

    module5_q23

--- src/main/java/ru/evol/eProj/configuration/AppConfiguration.java
+++ src/main/java/ru/evol/eProj/configuration/AppConfiguration.java
@@ -1,6 +1,7 @@
 package ru.evol.eProj.configuration;
 
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -9,6 +10,7 @@ import org.springframework.web.client.RestTemplate;
 public class AppConfiguration {
 
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
--- src/main/java/ru/evol/eProj/controller/PersonController.java
+++ src/main/java/ru/evol/eProj/controller/PersonController.java
@@ -1,6 +1,7 @@
 package ru.evol.eProj.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,17 +15,19 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/person")
 public class PersonController {
-    @Autowired
-    private PersonRepository repository;
 
     @Autowired
+    private PersonRepository repository;
+    @Autowired
     private RestTemplate restTemplate;
+    @Value("${location.service.url}")
+    private String locationServiceUrl;
 
     @GetMapping("{id}/weather")
     public ResponseEntity<Weather> getWeather(@PathVariable int id) {
         if (repository.existsById(id)) {
             String location = repository.findById(id).get().getLocation();
-            Weather weather = restTemplate.getForObject("http://localhost:8083/weather?location=" + location, Weather.class);
+            Weather weather = restTemplate.getForObject(locationServiceUrl + location, Weather.class);
             return new ResponseEntity(weather, HttpStatus.OK);
         }
         return new ResponseEntity(null, HttpStatus.NOT_FOUND);
@@ -46,27 +49,5 @@ public class PersonController {
                 ? new ResponseEntity(repository.findById(person.getId()), HttpStatus.BAD_REQUEST)
                 : new ResponseEntity(repository.save(person), HttpStatus.CREATED);
     }
-
-//    @PostMapping("/person")
-//    public ResponseEntity<Person> createPerson(@RequestBody Person person) {
-//        Person savedPerson = repository.save(person);
-//        return new ResponseEntity(savedPerson, HttpStatus.CREATED);
-//    }
-
-
-//    @PutMapping("/persons/{id}")
-//    public ResponseEntity<Person> updatePerson(@PathVariable int id, @RequestBody Person person) {
-//        if (repository.existsById(id)) {
-//            person.setId(id);
-//            return new ResponseEntity(repository.save(person), HttpStatus.OK);
-//        } else {
-//            return new ResponseEntity(HttpStatus.BAD_REQUEST);
-//        }
-//    }
-//
-//    @DeleteMapping("/persons/{id}")
-//    public void deletePerson(@PathVariable int id) {
-//        repository.deleteById(id);
-//    }
 
 }
